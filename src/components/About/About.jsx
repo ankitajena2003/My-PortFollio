71ac5f3692388fe380754136f2af8e6185533784
Ankita Jena <ankitakjena2003@gmail.com>
2025-07-16 11:21:06 +0530

    Update About.jsx
    photo added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -76,7 +76,7 @@ const About = () => {
             gyroscope={true}
           >
             <img
-              src={profileImage}
+              src="/src/assets/work_logo/profile2.png"
               alt="Ankita Jena"
               className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
             />
